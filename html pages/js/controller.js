fdc044a41da4137132fe4bc84b513cb53b1eb9b1
ljsjack <lowjiansheng@hotmail.sg>
2016-06-08 23:35:48 +0800

    Updated routing of angular

--- html pages/js/controller.js
+++ html pages/js/controller.js
@@ -5,8 +5,33 @@ app.config(function($routeProvider){
         .when('/',{
             templateUrl: 'firstpage.html'
         })
-        .when('/anotherPage',{
-            template: 'Welcome user,again!'
+        .when('/mainpage',{
+            templateUrl: 'firstpage.html'
+        })
+
+        .when('/aboutus',{
+            templateUrl: 'aboutus.html'
+        })
+        .when('/signup',{
+            templateUrl: 'signup.html'
+        })
+        .when('/deck',{
+            templateUrl: 'deck.html'
+        })
+        .when('/terrace',{
+            templateUrl: 'terrace.html'
+        })
+        .when('/frontier',{
+            templateUrl: 'frontier.html'
+        })
+        .when('/technoedge',{
+            templateUrl: 'technoedge.html'
+        })
+        .when('/koufu',{
+            templateUrl: 'koufu.html'
+        })
+        .when('/foodclique',{
+            templateUrl: 'foodclique.html'
         })
         .otherwise({
             redirectTo: '/'
