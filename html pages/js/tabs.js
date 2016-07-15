2835bd47d957a3a4e34825fb4821ab9cb84e1099
Brehmer <Brehmerchan@gmail.com>
2016-07-15 12:13:20 +0800

    added tabs.js file and css format for deck

---
+++ html pages/js/tabs.js
@@ -0,0 +1,8 @@
+/**
+ * Created by BrehmerChan on 26/6/2016.
+ */
+
+
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
