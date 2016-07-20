d84bd233e49fc197b6ec0b329e7aeb5062af0a1c
ljsjack <lowjiansheng@hotmail.sg>
2016-07-20 15:26:01 +0800

    test

--- server.js
+++ server.js
@@ -4,3 +4,9 @@ var app = express();
 app.use(express.static(__dirname + "/htmlpages"));
 app.listen(process.env.PORT || 3000);
 console.log("Server up on port 3000");
+
+
+app.use(function(req, res) {
+    res.sendFile(__dirname + '/htmlpages/index.html');
+});
+
