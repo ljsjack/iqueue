79fbe6654ec26f14c1ddb5d9e79c395436a3847d
ljsjack <lowjiansheng@hotmail.sg>
2016-07-28 18:06:04 +0800

    Added MongoDB database

---
+++ model/orderSchema.js
@@ -0,0 +1,13 @@
+
+// Dependencies
+var restful = require('node-restful');
+var mongoose = restful.mongoose;
+
+var OrderSchema = new mongoose.Schema({
+    username: String,
+    orders: [{food: String}, {qty: Number}]
+});
+
+
+// Return model
+module.exports = restful.model('Orders', OrderSchema);
---
+++ routes/api.js
@@ -0,0 +1,51 @@
+// Dependencies
+var express = require('express');
+var router = express.Router();
+
+var mongoose = require('mongoose');
+var order = require('../model/orderSchema.js');
+
+// GET the whole database
+router.get('/', function(req, res, next) {
+    order.find(function (err, api) {
+        if (err) return next(err);
+        res.json(api);
+    });
+});
+
+// POST. Create an order.
+router.post('/', function(req, res, next) {
+    order.create(req.body, function (err, post) {
+        if (err) return next(err);
+        res.json(post);
+    });
+});
+
+
+// GET an individual
+router.get('/:id', function(req, res, next) {
+    order.findById(req.params.id, function (err, post) {
+        if (err) return next(err);
+        res.json(post);
+    });
+});
+
+/* PUT /todos/:id */
+router.put('/:id', function(req, res, next) {
+    order.findByIdAndUpdate(req.params.id, req.body, function (err, post) {
+        if (err) return next(err);
+        res.json(post);
+    });
+});
+
+/* DELETE /todos/:id */
+router.delete('/:id', function(req, res, next) {
+    order.findByIdAndRemove(req.params.id, req.body, function (err, post) {
+        if (err) return next(err);
+        res.json(post);
+    });
+});
+
+
+
+module.exports = router;
